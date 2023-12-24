d961fadcc1999f6a5931c03fd80bd670f8e585d1
Gokul <122973682+Gblnn@users.noreply.github.com>
2023-12-24 13:41:35 +0400

    Update chat.tsx

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -9,7 +9,7 @@ export default function ChatInput(props:Props){
     return(
         <>
         <Typography style={{textTransform:"uppercase",fontSize:"0.75rem", fontWeight:700}}>{props.author}</Typography>
-        <Typography style={{fontFamily:"Supreme",marginBottom:"0.5rem",padding:"0.25rem",background:"rgb(245,245,245)", borderLeft:"4px solid black",paddingLeft:"0.5rem",fontSize:"15px"}}>{props.message}</Typography>
+        <Typography style={{fontFamily:"Supreme",marginBottom:"0.5rem",padding:"0.25rem",background:"rgb(245,245,245)", borderLeft:"4px solid black",paddingLeft:"0.5rem",fontSize:"16px"}}>{props.message}</Typography>
         </>
     )
 }
