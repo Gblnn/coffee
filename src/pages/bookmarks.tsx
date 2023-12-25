6497d9d7ace02c00719a41b28bbf35cb463ca530
Gokul <122973682+Gblnn@users.noreply.github.com>
2023-12-25 20:13:49 +0400

    update

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -31,24 +31,27 @@ export default function bookmarks(){
             <div >
                 <InfiniteScroll
                 style={{padding:"1rem"}}
-                className='procard_container'
+                className='container'
                 dataLength={1}
                 next={()=>{}}
-                hasMore={true}
+                hasMore={false}
                 loader={<div className='loader'><LoadingOutlined style={{fontSize:"2rem"}}/></div>}
                 endMessage={
                     <div className='loader'>
                         <p style={{ textAlign: 'center' }}>
-                            <b>You're all caught up</b>
+                            <b>You've reached the end</b>
                         </p>
                     </div>  
-                      }>                 
-                    {
+                      }>
+                        <div className='procard_container'>
+                        {
                         posts==null?<Empty/>:
                     posts.map((posts)=>(
                         <Post id={posts.id}  key={posts.id} profile={posts.profile} author={posts.author} date={posts.date} content={posts.content} likes={posts.likes} comments={posts.comments} bookmarks={posts.bookmarks} colorscheme={posts.colorscheme} admin="" liked={posts.liked} bookmarked={posts.bookmarked}/>
                     ))
                     }
+                            </div>                 
+                    
                 </InfiniteScroll>         
             </div>
         </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import Post from '../components/post';
 import { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import PostAction from '../components/postaction.tsx';
+import {LoadingOutlined} from '@ant-design/icons'
 
 
 export default function Home(){
@@ -27,24 +28,27 @@ export default function Home(){
             <div >
                 <InfiniteScroll
                 style={{padding:"1rem"}}
-                className='procard_container'
+                className='container'
                 dataLength={1}
                 next={()=>{}}
                 hasMore={true}
-                loader=""
-                // loader={<div className='loader'><LoadingOutlined style={{fontSize:"2rem"}}/></div>}
+                
+                loader={<div className='loader'><LoadingOutlined style={{fontSize:"2rem"}}/></div>}
                 endMessage={
                     <div className='loader'>
                         <p style={{ textAlign: 'center' }}>
                             <b>You're all caught up</b>
                         </p>
                     </div>  
-                      }>                 
+                      }>
+                    <div className='procard_container'>
                     {
                     posts.map((posts)=>(
                         <Post id={posts.id}  key={posts.id} profile={posts.profile} author={posts.author} date={posts.date} content={posts.content} likes={posts.likes} comments={posts.comments} bookmarks={posts.bookmarks} colorscheme={posts.colorscheme} admin="" liked={posts.liked} bookmarked={posts.bookmarked} />
                     ))
                     }
+                        </div>                 
+                    
                     
                 </InfiniteScroll>
                 <div className='big_empty'></div>         
