1e5dd17cb676d777ad1890d1e7a4af8f92c92cde
Gokul <122973682+Gblnn@users.noreply.github.com>
2023-12-27 23:28:04 +0400

    Update chat.tsx

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,6 +1,6 @@
-import { Button, Dropdown, Modal, Typography } from "antd";
-import {MoreOutlined} from '@ant-design/icons'
-import { MenuProps } from "antd/lib";
+import { Typography } from "antd";
+// import {MoreOutlined} from '@ant-design/icons'
+// import { MenuProps } from "antd/lib";
 
 interface Props {
     id:string
@@ -11,36 +11,36 @@ interface Props {
 
 export default function ChatInput(props:Props){
 
-    const onDelete = () =>{
-        Modal.confirm({
-            centered:true,
-            title: "Delete Comment?",
-            content: 'This action can not be undone',
-            okText:"Delete",
-            cancelText:"Cancel",
-            onOk:handleDelete,
-            okButtonProps:{style:{background:"black"}},
-            cancelButtonProps:{style:{borderColor:"rgb(175,175,175)", color:"black"}}
-          });
-    }
+    // const onDelete = () =>{
+    //     Modal.confirm({
+    //         centered:true,
+    //         title: "Delete Comment?",
+    //         content: 'This action can not be undone',
+    //         okText:"Delete",
+    //         cancelText:"Cancel",
+    //         onOk:handleDelete,
+    //         okButtonProps:{style:{background:"black"}},
+    //         cancelButtonProps:{style:{borderColor:"rgb(175,175,175)", color:"black"}}
+    //       });
+    // }
 
-    const handleDelete = () => {
-        fetch("https://658c3fd2859b3491d3f5c978.mockapi.io/comments?postid="+props.id, {
-      method: 'DELETE',
-    })
-    }
+    // const handleDelete = () => {
+    //     fetch("https://658c3fd2859b3491d3f5c978.mockapi.io/comments?postid="+props.id, {
+    //   method: 'DELETE',
+    // })
+    // }
 
     // Dropdown Menu Items
-  const items: MenuProps['items'] = [
-    {
-      key: '1',
-      label: (
-        <a onClick={onDelete}>
-          Delete Post 
-        </a>
-      ),
-    }
-];
+//   const items: MenuProps['items'] = [
+//     {
+//       key: '1',
+//       label: (
+//         <a onClick={onDelete}>
+//           Delete Post 
+//         </a>
+//       ),
+//     }
+// ];
     return(
         <>
         <div id={props.id}>
@@ -48,11 +48,12 @@ export default function ChatInput(props:Props){
         <Typography style={{textTransform:"uppercase",fontSize:"0.75rem", fontWeight:700}}>{props.author}</Typography>
         <p style={{fontSize:"0.6rem"}}>{props.date}</p>
         </div>
-        <div className="chat_option_btn" style={{display:"flex",marginBottom:"0.5rem", justifyContent:"space-between", alignItems:"center"}}>
+        <div className="chat_option_btn" style={{display:"flex",marginBottom:"0.5rem", justifyContent:"space-between", alignItems:"center", background:"rgb(245,245,245)"}}>
         <Typography style={{fontFamily:"Supreme",padding:"0.25rem", borderLeft:"4px solid black",paddingLeft:"0.5rem",fontSize:"16px"}}>{props.message}</Typography>
-        <Dropdown trigger={['click']} menu={{items}} placement="topLeft">
+        
+        {/* <Dropdown trigger={['click']} menu={{items}} placement="topLeft">
         <Button className="no_bg no_border"><MoreOutlined/></Button>
-        </Dropdown>
+        </Dropdown> */}
         
         </div>
         
