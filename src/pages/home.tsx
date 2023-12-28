3150d151de0a3bf1c1300d2cd3f68f150c7a1639
Gokul <122973682+Gblnn@users.noreply.github.com>
2023-12-28 19:02:32 +0400

    update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -50,8 +50,8 @@ export default function Home(){
     
     return(
         <>
-        <div style={{marginRight:"2rem",right:0,position:"fixed",display:"flex", justifyContent:"flex-end"}}>
-            <div className='hoverable' style={{userSelect:"none",boxShadow:"1px 1px 5px rgb(200,200,200)",alignItems:"center",justifyContent:"center",display:"flex", gap:"0.25rem", paddingLeft:"0.75rem",paddingRight:"0.75rem", border:"1px solid rgb(220,220,220)", borderRadius:"1rem"}}>
+        <div className='userid'>
+            <div className='hoverable' style={{zIndex:10,userSelect:"none",boxShadow:"1px 1px 5px rgb(200,200,200)",alignItems:"center",justifyContent:"center",display:"flex", gap:"0.25rem", paddingLeft:"0.75rem",paddingRight:"0.75rem", border:"1px solid rgb(220,220,220)", borderRadius:"1rem"}}>
                 <img src='/coffee.png' style={{width:"1.2rem"}}></img>
                 <Typography style={{fontFamily:"Supreme",fontSize:"0.9rem", fontWeight:"600"}}>{user_data}</Typography>
             </div>
